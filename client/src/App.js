2eef07945f15a48dcd7c0878f99efcc971c759f2
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-02-01 23:25:53 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { StreamChat } from "stream-chat";
+import { Chat } from "stream-chat-react";
+import Cookies from "universal-cookie";
+
+const apiKey = "fvzspsmy2jtk";
+const client = StreamChat.getInstance(apiKey);
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="app_wrapper">
+      <Chat client={client} theme="team light">
+        <ChanelListContainer />
+        <ChannelContainer />
+      </Chat>
+    </div>
+  );
 }
 
 export default App;
